04920809a694550788509635f9ffeb80782cf77f
Матвей Волков <flexbookmatvey@icloud.com>
2021-07-27 20:03:47 +0500

    Try False Ready

--- TrueFalseInterface/Program.cs
+++ TrueFalseInterface/Program.cs
@@ -8,6 +8,7 @@ namespace TrueFalseInterface
 {
     static class Program
     {
+        // Волков Матвей
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
